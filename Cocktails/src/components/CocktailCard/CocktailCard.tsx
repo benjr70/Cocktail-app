f1467326b7889c6848f96437af68a463471949cf
Ben Rolf <benrolf70@gmail.com>
2021-06-20 17:31:25 -0400

    little card style

--- Cocktails/src/components/CocktailCard/CocktailCard.tsx
+++ Cocktails/src/components/CocktailCard/CocktailCard.tsx
@@ -1,11 +1,25 @@
 import { Cocktail } from "../../services/CocktailAPI"
-
+import './CocktailCard.css'
 
 export function CocktailCard(props: any){
+
     return (<div>
         {props.Cocktaillist.map((cocktail: Cocktail) =>{
             return (
-            <h2>{cocktail.drinkName}</h2>
+            <>
+            <div className='card'>
+                <h2>{cocktail.drinkName}</h2>
+                <img 
+                    src={cocktail.thumbNail}
+                    alt={cocktail.drinkName + 'ThumbNail'}
+                    width='200'
+                    ></img>
+                <p>Category: {cocktail.category}</p>
+                <p>Alcholic: {cocktail.alcoholic}</p>
+                <p>Glass Type: {cocktail.glassType}</p>
+                <p>How To: {cocktail.instructions}</p>
+            </div>
+            </>
             )
         })}
     </div>
