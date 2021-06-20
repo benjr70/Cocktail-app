0f5b9055570af2196b16d43f2fa41c8ff9711702
Ben Rolf <benrolf70@gmail.com>
2021-06-20 16:48:48 -0400

    mapped ingredients and measurements

--- Cocktails/src/services/CocktailAPI.ts
+++ Cocktails/src/services/CocktailAPI.ts
@@ -6,7 +6,7 @@ export interface Cocktail {
     category:string;
     alcoholic: boolean;
     instructions: string;
-    ingrediants: string[];
+    ingredients: string[];
     measurements: string [];
     thumbNail: string;
 }
@@ -16,7 +16,6 @@ export class CocktailAPI{
     public getCocktailbyName(name: string): Promise<Cocktail[]>{
         const baseurl = 'https://www.thecocktaildb.com/api/json/v1/1';
         const url = 'search.php?s=' + name
- 
          return axios({baseURL: baseurl, method: 'get', url: url}).then(response => {
             return this.mapData(response.data);
         })
@@ -33,8 +32,8 @@ export class CocktailAPI{
                 category:cocktail.strCategory,
                 alcoholic: cocktail.strAlcoholic,
                 instructions: cocktail.strInstructions,
-                ingrediants: cocktail.strIngredient1,
-                measurements: cocktail.strMeasure1,
+                ingredients: this.mapIngredients(cocktail).filter(ingredient => ingredient != null),
+                measurements: this.mapMeasurements(cocktail).filter(measurement => measurement != null),
                 thumbNail: cocktail.strDrinkThumb,
             }
             return mappedCocktail;
@@ -42,6 +41,18 @@ export class CocktailAPI{
         } else {
             return [];
         }
+    }
+
+    mapIngredients(drink: any): string[]{
+        return [drink.strIngredient1, drink.strIngredient2, drink.strIngredient3, drink.strIngredient4, drink.strIngredient5,
+            drink.strIngredient6, drink.strIngredient7, drink.strIngredient8, drink.strIngredient9, drink.strIngredient10,
+            drink.strIngredient11, drink.strIngredient12, drink.strIngredient13, drink.strIngredient14, drink.strIngredient15]
+    }
+
+    mapMeasurements(drink: any): string[]{
+        return [drink.strMeasure1, drink.strMeasure2, drink.strMeasure3, drink.strMeasure4, drink.strMeasure5,
+            drink.strMeasure6, drink.strMeasure7, drink.strMeasure8, drink.strMeasure9, drink.strMeasure10,
+            drink.strMeasure11, drink.strMeasure12, drink.strMeasure13, drink.strMeasure14, drink.strMeasure16,];
     }
 }
 
